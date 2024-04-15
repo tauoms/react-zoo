067ab638d81aab78e3e3a23149c63af5cf2da487
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-15 14:26:35 +0300

    Add capitalization of animal name in Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Card = ({ name }) => {
+  let capitalizedName = name.slice(0, 1).toUpperCase() + name.slice(1);
   return (
     <div className="card">
       <img src={`https://source.unsplash.com/random/400×400/?${name}`} />
-      <h2>{name}</h2>
+      <h2>{capitalizedName}</h2>
     </div>
   );
 };
