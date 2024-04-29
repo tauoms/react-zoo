6d9b0c71d2beda8748adcf85f262ddbcfafcd5aa
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-29 21:18:07 +0300

    Style HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,21 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      <Link to="/animals">Animals</Link>
-      <Link to="/birds">Birds</Link>
+    <div id="HomePage">
+      <Link to="/animals" className="hplink">
+        Animals
+      </Link>
+      <Link to="/birds" className="hplink">
+        Birds
+      </Link>
+      <img
+        src={`https://source.unsplash.com/random/400x800/?mammal`}
+        alt={`Photo of an animal`}
+      />
+      <img
+        src={`https://source.unsplash.com/random/400x800/?bird`}
+        alt={`Photo of a bird`}
+      />
     </div>
   );
 };
