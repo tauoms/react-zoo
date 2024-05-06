a891c78e34c71bbdbc1f0b98110825a0017cfcc9
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-05-06 13:22:34 +0300

    Add image to AnimalPage

--- src/routes/AnimalPage.jsx
+++ src/routes/AnimalPage.jsx
@@ -3,7 +3,17 @@ import { useParams } from "react-router-dom";
 const AnimalPage = () => {
   let { animalName } = useParams();
 
-  return <h2>{animalName} Page</h2>;
+  return (
+    <>
+      <h2>{animalName} Page</h2>
+      <div className="imgcontainer">
+        <img
+          src={`https://source.unsplash.com/random/400×400/?${animalName}`}
+          alt={`Photo of ${animalName}`}
+        />
+      </div>
+    </>
+  );
 };
 
 export default AnimalPage;
