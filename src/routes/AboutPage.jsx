fc554b5b09b236f61b83c7a8fb007d04c23e7ab3
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-06-01 19:27:18 +0300

    Amend AboutPage text content

--- src/routes/AboutPage.jsx
+++ src/routes/AboutPage.jsx
@@ -3,14 +3,17 @@ const AboutPage = () => {
     <div id="AboutPage">
       <h2>About</h2>
       <p>
-        This is a practice React app with add/remove likes, remove card and
-        search functionality. Animals can be browsed by category. Each animal
-        has its own page (click "More info" in card) with a back button. Info
-        paragraph on animal page is fetched from wikipedia.
+        <strong>This is a practice React app</strong> with add/remove likes,
+        remove card and search functionality. Animals can be browsed by
+        category. Each animal has its own page (click "More info" in card) with
+        a back button. Info paragraph on animal page is fetched from wikipedia.
       </p>
       <p>
-        Some of the new things I learned during this project: How to use the
-        React router, how to pass and destructure props,
+        <strong>Some of the new things I learned during this project:</strong>{" "}
+        How to use the React router and set up a page structure, how to pass and
+        destructure props, how to use router useNavigate to create a back button
+        and how to deploy a react project to github pages using the gh-pages
+        package.
       </p>
       <p>
         The site gets all images randomly from{" "}
